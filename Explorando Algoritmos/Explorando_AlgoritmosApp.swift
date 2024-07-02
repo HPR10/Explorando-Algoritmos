a6f8b6119dd7536a0f0360e2e001f5ee47171339
Hugo Pinheiro <hugo_hp10@hotmail.com>
2024-07-02 18:41:03 -0300

    Initial Commit

---
+++ Explorando Algoritmos/Explorando_AlgoritmosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Explorando_AlgoritmosApp.swift
+//  Explorando Algoritmos
+//
+//  Created by Hugo Pinheiro  on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Explorando_AlgoritmosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
